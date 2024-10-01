667eb530589b5e90cba1a8da0cac11e654f4fb26
TamamTamar <tamar.tamam770@gmail.com>
2024-10-01 10:26:28 +0300

    first commit

---
+++ src/validations/patterns.tsx
@@ -0,0 +1,16 @@
+export const urlPattern =
+    /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/;
+
+export const emailPattern = /\S+@gmail\.\S+/;
+
+export const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*-]).{9,20}/;
+const phonePattern = /^0[0-9]{9}$/;
+
+const patterns = {
+    url: urlPattern,
+    phone: phonePattern,
+    email: emailPattern,
+    password: passwordPattern,
+}
+
+export default patterns;
